30118b5f6c10294f7c097694c3ddc9d17eda2069
Jordan Triplett <JordanT111596@gmail.com>
2021-01-27 23:30:03 -0500

    Application MVP reached

--- BankingApplication.java
+++ BankingApplication.java
@@ -9,6 +9,10 @@ public class BankingApplication {
     //My main method
     public static void main(String[] args) {
         
+        //Creates starter account
+        BankAccount starterAccount = new BankAccount("Moneybags", "42069");
+        //Starts up the banking menu for the user
+        starterAccount.showMenu();
     }
 }
 
@@ -47,11 +51,11 @@ class BankAccount {
     void getPreviousTransaction () {
         //If transaction was a positive number
         if (previousTransaction > 0) {
-        System.out.println("Deposited: " + previousTransaction);
+        System.out.println("You Deposited: " + previousTransaction);
         }
         //If transaction was a negative number
         else if (previousTransaction < 0) {
-            System.out.println("Withdrew: " + Math.abs(previousTransaction));
+            System.out.println("You Withdrew: " + Math.abs(previousTransaction));
         } //If transaction was zero
         else {
             System.out.println("No transaction made!");
@@ -75,6 +79,71 @@ class BankAccount {
         System.out.println("C. Withdraw");
         System.out.println("D. Previous Transaction");
         System.out.println("E. Exit");
+
+        //do while loop for taking user choices
+        do {
+            System.out.println("===========================================================================");
+            System.out.println("Enter an option");
+            System.out.println("===========================================================================");
+            option = scanner.next().charAt(0);
+            System.out.println("\n");
+
+            switch (option) {
+
+                //Line to print balance
+                case 'A':
+                    System.out.println("---------------------------------------------------------------------------");
+                    System.out.println("Your current balance: " + balance);
+                    System.out.println("---------------------------------------------------------------------------");
+                    System.out.println("\n");
+                    break;
+
+                //After user selects deposit option
+                case 'B':
+                    System.out.println("---------------------------------------------------------------------------");
+                    System.out.println("How much would you like to deposit?");
+                    System.out.println("---------------------------------------------------------------------------");
+                    int amount = scanner.nextInt();
+                    deposit(amount);
+                    System.out.println("\n");
+                    break;
+
+                //After user selects withdraw option
+                case 'C':
+                    System.out.println("---------------------------------------------------------------------------");
+                    System.out.println("How much would you like to withdraw?");
+                    System.out.println("---------------------------------------------------------------------------");
+                    int amount2 = scanner.nextInt();
+                    withdraw(amount2);
+                    System.out.println("\n");
+                    break;
+
+                //Line to print previous transaction
+                case 'D':
+                    System.out.println("---------------------------------------------------------------------------");
+                    getPreviousTransaction();
+                    System.out.println("---------------------------------------------------------------------------");
+                    System.out.println("\n");
+                    break;
+
+                //Upon choosing the exit option...
+                case 'E':
+                    System.out.println("***************************************************************************");
+                    break;
+
+                //If the input is anything other than a given option
+                default:
+                    System.out.println("Please select either A, B, C, D, or E!");
+                    break;
+
+            }
+        }
+
+        //As long as the user doesn't choose to exit
+        while (option != 'E');
+
+        //Ending thank you message
+        System.out.println("Thank you for banking with Jordan's Java!");
 
 
     }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# BankingApplication
-Practicing Java
+# Banking Application
+This project uses java to simulate a simple banking application and gives me a little something to practice and brush up on my skills with.
