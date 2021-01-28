35b44fab370f299ae1454b602d9b9fce721353c0
JordanT111596 <JordanT111596@gmail.com>
2021-01-28 11:45:13 -0500

    Formatting fixes done on laptop

--- BankingApplication.java
+++ BankingApplication.java
@@ -5,72 +5,74 @@ import java.util.Scanner;
 
 //Full App Class
 public class BankingApplication {
-    
-    //My main method
+
+    // My main method
     public static void main(String[] args) {
-        
-        //Creates starter account
+
+        // Creates starter account
         BankAccount starterAccount = new BankAccount("Moneybags", "42069");
-        //Starts up the banking menu for the user
+        // Starts up the banking menu for the user
         starterAccount.showMenu();
     }
 }
 
-//Class used for creating bank account objects
+// Class used for creating bank account objects
 class BankAccount {
-    //variables for account
+    // variables for account
     int balance;
     int previousTransaction;
     String customerName;
     String customerId;
 
-    //Bank Account Constructor
+    // Bank Account Constructor
 
     BankAccount(String custName, String custId) {
         customerName = custName;
         customerId = custId;
     }
 
-    //Method for making deposits
-    void deposit (int amount) {
-        if (amount !=0) {
+    // Method for making deposits
+    void deposit(int amount) {
+        if (amount != 0) {
             balance = balance + amount;
             previousTransaction = amount;
         }
     }
 
-    //Method for making withdrawls
-    void withdraw (int amount) {
-        if (amount !=0) {
+    // Method for making withdrawls
+    void withdraw(int amount) {
+        if (amount != 0) {
             balance = balance - amount;
             previousTransaction = -amount;
         }
     }
 
-    //Method for finding previous transaction
-    void getPreviousTransaction () {
-        //If transaction was a positive number
+    // Method for finding previous transaction
+    void getPreviousTransaction() {
+        // If transaction was a positive number
         if (previousTransaction > 0) {
-        System.out.println("You Deposited: " + previousTransaction);
+            System.out.println("You Deposited: " + previousTransaction);
         }
-        //If transaction was a negative number
+        // If transaction was a negative number
         else if (previousTransaction < 0) {
             System.out.println("You Withdrew: " + Math.abs(previousTransaction));
-        } //If transaction was zero
+        } // If transaction was zero
         else {
             System.out.println("No transaction made!");
         }
     }
 
-    //Method for showing the menu
+    // Method for showing the menu
     void showMenu() {
 
-        //initializes the choice character
-        char option ='\0';
-        //Creates new scanner
+        // initializes the choice character
+        char option = '\0';
+        // Creates new scanner
         Scanner scanner = new Scanner(System.in);
 
-        //Menu printing
+        // Menu printing
+        System.out.println("\n");
+        System.out.println("\n");
         System.out.println("Welcome: " + customerName);
         System.out.println("Your ID is: " + customerId);
         System.out.println("\n");
@@ -80,7 +82,7 @@ class BankAccount {
         System.out.println("D. Previous Transaction");
         System.out.println("E. Exit");
 
-        //do while loop for taking user choices
+        // do while loop for taking user choices
         do {
             System.out.println("===========================================================================");
             System.out.println("Enter an option");
@@ -90,7 +92,7 @@ class BankAccount {
 
             switch (option) {
 
-                //Line to print balance
+                // Line to print balance
                 case 'A':
                     System.out.println("---------------------------------------------------------------------------");
                     System.out.println("Your current balance: " + balance);
@@ -98,7 +100,7 @@ class BankAccount {
                     System.out.println("\n");
                     break;
 
-                //After user selects deposit option
+                // After user selects deposit option
                 case 'B':
                     System.out.println("---------------------------------------------------------------------------");
                     System.out.println("How much would you like to deposit?");
@@ -108,7 +110,7 @@ class BankAccount {
                     System.out.println("\n");
                     break;
 
-                //After user selects withdraw option
+                // After user selects withdraw option
                 case 'C':
                     System.out.println("---------------------------------------------------------------------------");
                     System.out.println("How much would you like to withdraw?");
@@ -118,7 +120,7 @@ class BankAccount {
                     System.out.println("\n");
                     break;
 
-                //Line to print previous transaction
+                // Line to print previous transaction
                 case 'D':
                     System.out.println("---------------------------------------------------------------------------");
                     getPreviousTransaction();
@@ -126,12 +128,12 @@ class BankAccount {
                     System.out.println("\n");
                     break;
 
-                //Upon choosing the exit option...
+                // Upon choosing the exit option...
                 case 'E':
                     System.out.println("***************************************************************************");
                     break;
 
-                //If the input is anything other than a given option
+                // If the input is anything other than a given option
                 default:
                     System.out.println("Please select either A, B, C, D, or E!");
                     break;
@@ -139,12 +141,11 @@ class BankAccount {
             }
         }
 
-        //As long as the user doesn't choose to exit
+        // As long as the user doesn't choose to exit
         while (option != 'E');
 
-        //Ending thank you message
+        // Ending thank you message
         System.out.println("Thank you for banking with Jordan's Java!");
-
 
     }
 }
